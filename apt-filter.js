810bc4b88e3e58e9f17567677e09640ed32beb80
Or <orlevy11@gmail.com>
2022-08-27 18:24:46 +0300

    Done

--- apt-filter.js
+++ apt-filter.js
@@ -1,16 +1,28 @@
 //Only start playing with this file if you're done with all the basics
 
-const must = (radio) => radio.filter(rb => rb.checked).length
+const must = (radio) => radio.filter((rb) => rb.checked).length;
 
-const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRooms, immediate) {
-    let relevantApts = apartments.filter(a =>
-        a.address.toLowerCase().includes(address.toLowerCase() || "") &&
-        a.price >= (minPrice || 0) &&
-        a.price <= (maxPrice || a.price) &&
-        a.rooms >= (minRooms || 0) &&
-        a.rooms <= (maxRooms || a.rooms)
-    )
-
-    immediate = !immediate[0].checked
-    return immediate ? relevantApts : relevantApts.filter(a => (a.immediate)) 
-}
+const findRelevantApts = function (
+  address,
+  minPrice,
+  maxPrice,
+  minRooms,
+  maxRooms,
+  immediate,
+  park
+) {
+  let relevantApts = apartments.filter(
+    (a) =>
+      a.address.toLowerCase().includes(address.toLowerCase() || "") &&
+      a.price >= (minPrice || 0) &&
+      a.price <= (maxPrice || a.price) &&
+      a.rooms >= (minRooms || 0) &&
+      a.rooms <= (maxRooms || a.rooms)
+  );
+  parking = park[0].checked;
+  if (parking) {
+    relevantApts = relevantApts.filter((a) => a.parking);
+  }
+  immediate = !immediate[0].checked;
+  return immediate ? relevantApts : relevantApts.filter((a) => a.immediate);
+};
